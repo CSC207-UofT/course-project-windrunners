9163b8b1b4669cc6644e519bb3f72aa67026293f
Elliot Schrider <70617326+elliotschrider@users.noreply.github.com>
2021-10-16 13:21:49 -0400

    Add player methods, and beginning of Game

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,9 +1,51 @@
 package main.java;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
     private String name;
     private int points;
     private List<Tile> rack;
+
+    public Player(String name) {
+        this.name = name;
+        this.points = 0;
+        this.rack = new ArrayList<>();
+    }
+    public void addPoints(int value) {
+        points += value;
+    }
+
+    public void addTile(Tile tile) {
+        rack.add(tile);
+    }
+
+    public void removeTile(char letter) {
+        for (Tile tile : rack) {
+            if (tile.getLetter() == letter) {
+                rack.remove(tile);
+            }
+        }
+    }
+
+    public int getRackSize() {
+        return rack.size();
+    }
+
+    public boolean hasLetters(List<Character> letterList) {
+        boolean hasLetter;
+        for (char letter : letterList) {
+            hasLetter = false;
+            for (Tile tile : rack) {
+                if (tile.getLetter() == letter) {
+                    hasLetter = true;
+                }
+            }
+            if (!hasLetter) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
