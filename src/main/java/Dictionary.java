0f2f952459014ed7ce7f74571aa66dbfca78c371
Elliot Schrider <70617326+elliotschrider@users.noreply.github.com>
2021-10-16 10:48:53 -0400

    Add methods to Dictionary

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -1,9 +1,33 @@
 package main.java;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+
 public class Dictionary {
-    private String[] dictionary;
+    private HashSet<String> dictionary;
 
     public Dictionary() {
-        
+        dictionary = new HashSet<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("./dictionary.txt"))) {
+            String line;
+            int i = 0;
+            while ((line = br.readLine()) != null) {
+                dictionary.add(line);
+                i++;
+            }
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.out.println("File Not Found");
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public HashSet<String> getDictionary() { return dictionary; }
+
+    public boolean inDictionary(String word) {
+        return dictionary.contains(word);
     }
 }
