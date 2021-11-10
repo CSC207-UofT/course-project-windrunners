7a71ee00aa27b2bfcce134f8e00232b340069639
shaulfrancus <shaul.francus@mail.utoronto.ca>
2021-11-10 18:15:14 -0500

    added multipliers/modifiers for Squares

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -2,15 +2,30 @@ package main.java;
 
 public class Square {
     private Tile tile;
-    private final String modifier;
+    private final int letterMult;
+    private final int wordMult;
+    private boolean multActive;
 
     public Square() {
         tile = null;
-        modifier = "none";
+        multActive = true;
+        letterMult = 1;
+        wordMult = 1;
+    }
+
+    public Square(int letterMult, int wordMult) {
+        tile = null;
+        multActive = true;
+        this.letterMult = letterMult;
+        this.wordMult = wordMult;
     }
 
     public Tile getTile() { return tile; }
     public void setTile(Tile tile) { this.tile = tile; }
 
-    public String getModifier() { return modifier; }
+    public boolean isMultActive() { return multActive; }
+
+    public void multUsed() { multActive = false; }
+    public int getLetterMult() { return letterMult; }
+    public int getWordMult() { return wordMult; }
 }
