440796955a4dea145d3bc31d27959cbfa10c5205
shaulfrancus <shaul.francus@mail.utoronto.ca>
2021-11-12 20:21:12 -0500

    Refactored Tile valueDict and Board letterDistribution

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -2,16 +2,15 @@ package main.java;
 
 import java.util.*;
 
+import static java.util.Map.entry;
 public class Bag {
 
     private final List<Tile> tiles = new ArrayList<>();
 
     public Bag() {
-        HashMap<Character, Integer> distribution = getLetterDistribution();
-        for (int i = 65; i <= 90; i++) {
-            int num = distribution.get((char) i);
-            for (int j = 0; j < num; j++) {
-                tiles.add(new Tile((char) i));
+        for (Map.Entry<Character,Integer> en : LETTER_DISTRIBUTION.entrySet()) {
+            for (int j = 0; j < en.getValue(); j++) {
+                tiles.add(new Tile(en.getKey()));
             }
         }
     }
@@ -30,36 +29,41 @@ public class Bag {
         for (int i = 0; i < numTilesToSwap; i++) {
             tilesReturned.add(drawTile());
         }
-        for (int k = 0; k < numTilesToSwap; k++) {
-            tiles.add(tilesToSwap.get(k));
-        }
+        tiles.addAll(tilesToSwap);
         return tilesReturned;
-    }
-
-    private HashMap<Character, Integer> getLetterDistribution() {
-        HashMap<Character, Integer> letterDistribution = new HashMap<>();
-        for (char letter : new char[]{'J', 'K', 'Q', 'X', 'Z'}) {
-            letterDistribution.put(letter, 1);
-        }
-        for (char letter : new char[]{'B', 'C', 'F', 'H', 'M', 'P', 'V', 'W', 'Y'}) {
-            letterDistribution.put(letter, 2);
-        }
-        letterDistribution.put('G', 3);
-        for (char letter : new char[]{'D', 'L', 'S', 'U'}) {
-            letterDistribution.put(letter, 4);
-        }
-        for (char letter : new char[]{'N', 'R', 'T'}) {
-            letterDistribution.put(letter, 6);
-        }
-        letterDistribution.put('O', 8);
-        letterDistribution.put('A', 9);
-        letterDistribution.put('I', 9);
-        letterDistribution.put('E', 12);
-
-        return letterDistribution;
     }
 
     public int numTilesRemaining() {
         return tiles.size();
     }
+
+    private static final Map<Character, Integer> LETTER_DISTRIBUTION = Map.ofEntries(
+            entry('A', 9),
+            entry('B', 2),
+            entry('C', 2),
+            entry('D', 4),
+            entry('E', 12),
+            entry('F', 2),
+            entry('G', 3),
+            entry('H', 2),
+            entry('I', 9),
+            entry('J', 1),
+            entry('K', 1),
+            entry('L', 4),
+            entry('M', 1),
+            entry('N', 6),
+            entry('O', 8),
+            entry('P', 2),
+            entry('Q', 1),
+            entry('R', 6),
+            entry('S', 4),
+            entry('T', 6),
+            entry('U', 4),
+            entry('V', 2),
+            entry('W', 2),
+            entry('X', 1),
+            entry('Y', 1),
+            entry('Z', 1)
+    );
+
 }
