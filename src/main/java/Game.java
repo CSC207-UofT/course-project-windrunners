f080a48f6e7af5fd2e4eb4d184f4ede1f4b97af3
Shridhar Shatrughan <63154973+shribyte@users.noreply.github.com>
2021-11-14 22:46:36 -0500

    Reformat code and javadoc, Shorten function calls.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -9,17 +9,17 @@ import java.util.List;
 public class Game {
 
     /**
-     * The main method. Sets up and controls the state of the Game.
-     * The Game ends when the Bag empties.
+     * The main method. Sets up and controls the state of the Game
+     * The Game ends when the Bag empties
      */
     public static void main(String[] args) {
         Dictionary dictionary = new Dictionary();
         Bag bag = new Bag();
         System.out.println(bag.numTilesRemaining());
         Board board = new Board();
-        PlayerManager playerManager = new PlayerManager(System.in, System.out, bag);
+        PlayerManager playerManager = new PlayerManager(bag);
         while (bag.numTilesRemaining() > 0) {
-            Move move = playerManager.getNextMove(System.in, System.out, board, bag.numTilesRemaining());
+            Move move = playerManager.getNextMove(board, bag.numTilesRemaining());
             if (move.getMoveType().equals("SWAP")) {
                 handleSwapMove((SwapMove) move, bag, playerManager);
             } else if (move.getMoveType().equals("PLACE")) {
@@ -33,9 +33,10 @@ public class Game {
 
     /**
      * calls the Player Manager to enable the Player swap tiles with the Bag
+     *
      * @param move a SwapMove which stores the tiles to swap
-     * @param bag the bag to swap tiles with
-     * @param pm is the Player Manager
+     * @param bag  the bag to swap tiles with
+     * @param pm   is the Player Manager
      */
     public static void handleSwapMove(SwapMove move, Bag bag, PlayerManager pm) {
         List<Tile> tilesToSwap = move.getTilesToSwap();
@@ -44,13 +45,12 @@ public class Game {
     }
 
     /**
-     *
-     * @param move a PlaceMove which stores the word to be inserted on the board, its co-ordinates,
-     *         and the direction along which the word is to be inserted
-     * @param bag the bag to replenish the currentPlayer's rack after the move has been made
-     * @param pm the Player Manager
+     * @param move  a PlaceMove which stores the word to be inserted on the board, its co-ordinates,
+     *              and the direction along which the word is to be inserted
+     * @param bag   the bag to replenish the currentPlayer's rack after the move has been made
+     * @param pm    the Player Manager
      * @param board the Scrabble Board on which the word is to be inserted
-     * @param dict the Scrabble dictionary
+     * @param dict  the Scrabble dictionary
      */
     public static void handlePlaceMove(PlaceMove move, Bag bag, PlayerManager pm, Board board, Dictionary dict) {
         int x = move.getX(), y = move.getY();
@@ -69,7 +69,7 @@ public class Game {
         for (char c : lettersNeeded) {
             tilesForWord.add(new Tile(c));
         }
-        int points = board.insertWord(x,y,direction,tilesForWord);
+        int points = board.insertWord(x, y, direction, tilesForWord);
         List<Tile> tilesToAdd = bag.drawTiles(tilesForWord.size());
         pm.updateCurrentPlayer(points, tilesToAdd, tilesForWord);
     }
