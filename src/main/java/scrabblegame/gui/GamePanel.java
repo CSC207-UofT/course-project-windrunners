cddef0cf9f5e478906cac23d57d49f605c3ae663
Rachel Kim <66101337+rachelmkim@users.noreply.github.com>
2021-12-09 02:35:50 -0500

    Update GamePanel.java

--- src/main/java/scrabblegame/gui/GamePanel.java
+++ src/main/java/scrabblegame/gui/GamePanel.java
@@ -5,6 +5,9 @@ import main.java.scrabblegame.game.Game;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * The class representing the visual component of the GUI
+ */
 public class GamePanel extends JPanel {
     private static final int WIDTH = 800;
     private static final int HEIGHT = 800;
@@ -12,12 +15,20 @@ public class GamePanel extends JPanel {
     private static Game currGame;
     private static final Renderer renderer = new Renderer();
 
+    /**
+     * Class constructor for the GamePanel (the screen showing the Scrabble game that is being played)
+     * @param game the game that is currently being played
+     */
     public GamePanel(Game game) {
         super();
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         currGame = game;
     }
-
+    
+    /**
+     * Paints all the visual components required for the game on the GamePanel
+     * @param g the graphics object used to create the visual components of the board
+     */
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setFont(new Font("TimesRoman", Font.BOLD, 30));
